206dfa556afb167d1cba07a81b472c4429f7c2b0
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-12-01 19:24:58 -0500

    added constructor test

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -13,28 +13,30 @@ public class PersonTest {
         List<Person> people = new ArrayList<>();
         Person person1 = new Person("Amanda", LocalDate.of(1988, 12, 19), Person.Sex.FEMALE, "coolgirl@gmail.com");
         Person person2 = new Person("Hillary", LocalDate.of(1990, 01,01), Person.Sex.FEMALE, "skillary1@gmail.com");
+        people.add(person1);
+        people.add(person2);
     }
 
     @Test
     public void personConstructorTest(){
         //given
         String expectedName = "Pete";
-        LocalDate expectedBirthdate = LocalDate.now();
+        LocalDate expectedBirthDate = LocalDate.now();
         Person.Sex expectedSex = Person.Sex.MALE;
         String expectedEmailAddress = "pk@aol.com";
         //when
-        Person person = new Person(expectedName, expectedBirthdate, expectedSex, expectedEmailAddress);
+        Person person = new Person(expectedName, expectedBirthDate, expectedSex, expectedEmailAddress);
         person.setName(expectedName);
         String actualName = person.getName();
-        person.setBirthday(expectedBirthdate);
-        LocalDate actualBirthdate = person.getBirthday();
+        person.setBirthday(expectedBirthDate);
+        LocalDate actualBirthDate = person.getBirthday();
         person.setGender(expectedSex);
         Person.Sex actualSex = person.getGender();
         person.setEmailAddress(expectedEmailAddress);
         String actualEmailAddress = person.getEmailAddress();
         //then
         Assert.assertEquals(expectedName, actualName);
-        Assert.assertEquals(expectedBirthdate, actualBirthdate);
+        Assert.assertEquals(expectedBirthDate, actualBirthDate);
         Assert.assertEquals(expectedSex, actualSex);
         Assert.assertEquals(expectedEmailAddress, actualEmailAddress);
     }
